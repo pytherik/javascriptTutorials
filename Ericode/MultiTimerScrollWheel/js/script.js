84b1b2b6ffbb38950412309b5cb4d4bc6755a0b2
pytherik <erikberndt@gmx.net>
2023-07-04 21:41:55 +0200

    wheel performance array

--- Ericode/MultiTimerScrollWheel/js/script.js
+++ Ericode/MultiTimerScrollWheel/js/script.js
@@ -1,20 +1,35 @@
 'use strict';
 
-const setTimer = document.querySelector('.set-timer');
+const timeContainer = document.querySelector('.time-container');
 const hours = document.getElementById('hh');
 const mins = document.getElementById('mm');
 const secs = document.getElementById('ss');
 
+// timeContainer.addEventListener('wheel', (e) => {
+//   if (!(e.target.classList.contains('num'))) return;
+//   if (e.deltaY < 0) {
+//     +e.target.dataset.t++;
+//     if (+e.target.dataset.t > +e.target.dataset.max) e.target.dataset.t = '00';
+//   } else {
+//     +e.target.dataset.t--;
+//     if (+e.target.dataset.t < 0) e.target.dataset.t = e.target.dataset.max;
+//   }
+//   e.target.innerText = `0${e.target.dataset.t}`.slice(-2);
+// });
 
-
-setTimer.addEventListener('wheel', (e) => {
+timeContainer.addEventListener('wheel', (e) => {
   if (!(e.target.classList.contains('num'))) return;
-  e.deltaY < 0
-    ? e.target.dataset.t++
-    : e.target.dataset.t--;
-  if(+e.target.dataset.t > 59)  e.target.dataset.t = '00';
-  if(+e.target.dataset.t < 0)  e.target.dataset.t = '59';
+  const arr = [];
+  for(let i = 0; i < +e.target.dataset.max; i++) arr.push(`0${i}`.slice(-2));
+  if (e.deltaY < 0) {
+    +e.target.dataset.t++;
+    if (+e.target.dataset.t > +e.target.dataset.max) e.target.dataset.t = '00';
+  } else {
+    +e.target.dataset.t--;
+    if (+e.target.dataset.t < 0) e.target.dataset.t = e.target.dataset.max;
+  }
   e.target.innerText = `0${e.target.dataset.t}`.slice(-2);
+  console.log([...arr.slice(+e.target.dataset.t),
+    ...arr.slice(0,+e.target.dataset.t)]);
 });
-
 
